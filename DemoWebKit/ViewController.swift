1dca16a2c72fd43698a190f2987cf774033ac792
Nguyen Minh Tam <tamb1606932@student.ctu.edu.vn>
2017-11-28 22:35:55 +0700

    Added some property: Browse, back, refresh, forward, ... Allow load http

--- DemoWebKit/ViewController.swift
+++ DemoWebKit/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Outlet
+    @IBOutlet var txtURL: UITextField!
+    @IBOutlet var webView: UIWebView!
+    
+    // Action
+    @IBAction func Duyet(_ sender: Any) {
+        duyetWeb()
+    }
+    @IBAction func back(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    @IBAction func refesh(_ sender: Any) {
+        webView.reload()
+    }
+    @IBAction func forward(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    @IBAction func txtURLEDE(_ sender: Any) {
+        duyetWeb()
+    }
+    
+    func duyetWeb(){
+        if let uRL = URL(string: txtURL.text!) {
+            webView.loadRequest(URLRequest(url: uRL))
+            if !webView.isLoading {
+                webView.loadRequest(URLRequest(url: URL(string: "https://www.google.com.vn")!))
+            }
+        } else {
+            webView.loadRequest(URLRequest(url: URL(string: "https://www.google.com.vn")!))
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView.loadRequest(URLRequest(url: URL(string: "http://www.cit.ctu.edu.vn")!))
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +59,3 @@ class ViewController: UIViewController {
 
 
 }
-
